c398a1b0cf3c91442769fce1166e95b2d02c45cc
viveknfc <129741775+viveknfc@users.noreply.github.com>
2023-06-30 15:36:55 +0530

    New viv file added

---
+++ RadiusAgent/Cell/vivek.swift
@@ -0,0 +1,20 @@
+//
+//  vivek.swift
+//  RadiusAgent
+//
+//  Created by NFC User on 30/06/23.
+//
+
+import UIKit
+
+class vivek: UIActivityIndicatorView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
